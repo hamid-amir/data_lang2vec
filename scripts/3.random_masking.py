e3620260e45be2c64a72b5e016c9b6c036c98bf4
Hamidreza Amirzadeh <h.amirzadeh1378@gmail.com>
2024-05-15 00:50:19 +0330

    Implemented k-fold for random_masking and uploaded resulting folds

--- scripts/3.random_masking.py
+++ scripts/3.random_masking.py
@@ -1,45 +1,61 @@
+
+# In this script we only use non-missing (present) instances
+# of lang2vec syntax_wals+phonology_wals features of 1720
+# languages and create k-fold split for train and test set
+# using a RANDOM manner to ready the data for train and evaluate 
+# of the predcition classifiers. 
+# Important note: we just save names of the features as our folds,
+# NOT specificly x and y. This is due to the large size of x.
+# You can easily find the corresponding x and y for these names!
+
 import random
-from tqdm import tqdm
-import os
+import pickle
 import lang2vec.lang2vec as l2v
-
-
-miltale_langs = set()
-for lang_file in os.listdir('data/MILTALE-CLEAN/'):
-    lang_code = lang_file.split('.')[0]
-    miltale_langs.add(lang_code)
-
-lang2vec_langs = []
-for lang in l2v.LANGUAGES:
-   lang2vec_langs.append(lang)
-
-common_langs = list(set(miltale_langs) & set(lang2vec_langs))
+import myutils
+from sklearn.model_selection import KFold
 
 
 
-typo_source = 'syntax_wals'
-total, unmissing = 0, 0
-all_pres_syntax_wals = {}
+random.seed(myutils.seed)
 
-for lang in tqdm(common_langs):
-    sw_features_names = l2v.get_features(lang, typo_source, header=True)['CODE']
-    sw_features_values = l2v.get_features(lang, 'syntax_wals', header=True)[lang]
-    for name, value in zip(sw_features_names, sw_features_values):
-        if value != '--':
-            unmissing += 1
-            all_pres_syntax_wals[f'{lang}_{name}'] = value
-        total += 1
+# num_folds
+k = 10
+
+# this includes syntax_wals+phonology_wals data for 1720 languages
+langs, vectors, vectors_knn = pickle.load(open('lang2vec.pickle', 'rb'))
+
+feature_names = l2v.get_features('eng', 'syntax_wals+phonology_wals', header=True)['CODE']
+
+# select only present values
+y = []
+names = []
+for vector, lang in zip(vectors, langs):
+    for val, feature in zip(vector, feature_names):
+        if val != -100:
+            y.append(val)
+            names.append(lang + '|' + feature)
+
+print(f'we have {len(y)} present instances out of total {len(langs) * len(feature_names)} instances.')
+
+
+# shuffle
+z = [[val, name] for val, name in zip(y, names)]
+random.shuffle(z)
+
+y = [item[0] for item in z]
+names = [item[1] for item in z]
 
 
 
-seed = 42
-train_y, test_y, dev_y = {}, {}, {}
+kf = KFold(n_splits=k, shuffle=True, random_state=myutils.seed)
+folds = []
+for train_idx, test_idx in kf.split(z):
+    train_names = [names[i] for i in train_idx]
+    test_names = [names[i] for i in test_idx]
+    folds.append({'train_names': train_names, 'test_names':test_names})
 
-for key, value in all_pres_syntax_wals.items():
-    rand = random.random()
-    if rand < 0.7:
-        train_y[key] = value
-    elif 0.7 < rand < 0.85:
-        test_y[key] = value
-    else:
-        dev_y[key] = value
+
+with open('folds.pickle', 'wb') as f:
+    pickle.dump(folds, f)
+
+print(f'{k} folds of present instances were created and saved to the "folds.pickle" file.')
