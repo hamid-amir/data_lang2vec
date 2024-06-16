4ac165b23dfccca5649d506e9c6bd9681510d977
Sadegh <sadeghjafari5079@gmail.com>
2024-06-16 17:14:06 +0330

    add n_components to optuna hyper parameter tunning

--- find_missing/5.optuna.py
+++ find_missing/5.optuna.py
@@ -14,7 +14,11 @@ import scripts.myutils as myutils
 from find_missing.utils import cross_validation, get_clf
 
 
-def objective(trial, method: str, x, y):
+def objective(trial, method: str):
+    n_components = trial.suggest_int('n_components', 10, 100)
+    myutils.extract_features('find_missing', n_components=n_components, n=100)
+    x, y, names, all_feat_names = pickle.load(open('feats-full_find_missing.pickle', 'rb'))
+
     if method == 'svm':
         C = trial.suggest_loguniform('C', 1e-4, 1e3)
         kernel = trial.suggest_categorical('kernel', ['linear', 'poly', 'rbf', 'sigmoid'])
@@ -22,7 +26,7 @@ def objective(trial, method: str, x, y):
         gamma = trial.suggest_categorical('gamma', ['scale', 'auto'])
         clf = SVC(C=C, kernel=kernel, degree=degree, gamma=gamma)
     elif method == 'rf':
-        max_depth = trial.suggest_int('max_depth', 3, 100)
+        max_depth = trial.suggest_categorical('max_depth', [None] + list(range(3, 101)))
         min_samples_split = trial.suggest_int('min_samples_split', 2, 100)
         criterion = trial.suggest_categorical('criterion', ["gini", "entropy", "log_loss"])
         class_weight = trial.suggest_categorical('class_weight', [None, "balanced", "balanced_subsample"])
@@ -36,7 +40,7 @@ def objective(trial, method: str, x, y):
         solver = trial.suggest_categorical('solver', ['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga'])
         clf = LogisticRegression(penalty=penalty, tol=tol, C=C, solver=solver)
 
-    result = cross_validation(clf, x, y, print_result=False)
+    result = cross_validation(clf, x, y)
 
     return result['f1']
 
@@ -45,27 +49,15 @@ def hyperparameter_tuning():
     method = sys.argv[1]
     start_time = time.time()
 
-    x, y, names, all_feat_names = pickle.load(open('feats-full_find_missing.pickle', 'rb'))
-
     study = optuna.create_study(direction='maximize')
-    study.optimize(lambda trial: objective(trial, method, x, y), n_trials=100)
+    study.optimize(lambda trial: objective(trial, method), n_trials=10)
 
     best_trial = study.best_trial
     print(f'Best trial: {best_trial.values}')
-
-    clf = get_clf(method).set_params(**best_trial.params)
-    clf.fit(x, y)
-    result = cross_validation(clf, x, y)
-    result.update({'best_model': clf})
     seconds = time.time() - start_time
-    result['seconds'] = seconds
-
-    return result
+    print('seconds: {:.2f}'.format(seconds))
+    return best_trial.values
 
 
 if __name__ == '__main__':
-    for n_components in [0, 10, 30, 50, 70, 90]:
-        print(f'n_components: {n_components}')
-        myutils.extract_features('find_missing', n_components=n_components, n=100)
-        hyperparameter_tuning()
-        print('-'*40)
+    hyperparameter_tuning()
