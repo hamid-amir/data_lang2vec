bf761522adf81de9c524ddf05a132dea3f3c77d8
Sadegh <sadeghjafari5079@gmail.com>
2024-08-01 16:46:52 +0330

    write a optuna for each target features

--- find_value/3.optuna.py
+++ find_value/3.optuna.py
@@ -19,7 +19,7 @@ from find_missing.utils import cross_validation, get_clf
 
 
 
-def objective(trial, method: str):
+def objective(trial, method: str, target_feature: str):
     features = ['lang_id', 'feat_id', 'geo_lat', 'geo_long', 'lang_group', 'aes_status', 'wiki_size', 'num_speakers', 'lang_fam', 'scripts', 'feat_name', 'phylogency']
     remove_features = [trial.suggest_categorical(feature, [True, False]) for feature in features]
     remove_features = [feature for feature, selected in zip(features, remove_features) if selected]
@@ -34,12 +34,18 @@ def objective(trial, method: str):
         'find_value',
         n_components=n_components,
         miltate_n_components=miltate_n_components,
-        n=300,
+        # n=300,
         remove_features=remove_features,
         miltale_data=miltale_data
     )
 
-    x, y, names, all_feat_names = pickle.load(open('feats-full_find_value_mil.pickle', 'rb'))
+    X, Y, names, all_feat_names = pickle.load(open('feats-full_find_value_mil.pickle', 'rb'))
+
+    new_X, new_Y = list(), list()
+    for i, name in enumerate(names):
+        if name.split('|')[1] == target_feature:
+            new_X.append(X[i])
+            new_Y.append(Y[i])
 
     if method == 'svm':
         C = trial.suggest_float('C', 1e-4, 1e3, log=True)
@@ -89,7 +95,7 @@ def objective(trial, method: str):
                                      min_samples_leaf=min_samples_leaf, criterion=criterion)
         
     try:
-        result = cross_validation(clf, x, y)
+        result = cross_validation(clf, new_X, new_Y)
         return result['f1']
     except ValueError:
         return 0
@@ -97,20 +103,20 @@ def objective(trial, method: str):
     
 
 
-def hyperparameter_tuning(method: str, n_trails: int, save_dir: str = 'result'):
+def hyperparameter_tuning(method: str, n_trails: int, target_feature: str, save_dir: str = 'result'):
     start_time = time.time()
 
     study = optuna.create_study(direction='maximize')
-    study.optimize(lambda trial: objective(trial, method), n_trials=n_trails)
+    study.optimize(lambda trial: objective(trial, method, target_feature), n_trials=n_trails)
 
     optuna.visualization.matplotlib.plot_optimization_history(study)
 
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    plt.savefig(f'{save_dir}/optimization_history_{method}.png')
+    plt.savefig(f'{save_dir}/optimization_history_method_{method}_target_feature_{target_feature}.png')
     optuna.visualization.matplotlib.plot_slice(study)
-    plt.savefig(f'{save_dir}/slice_{method}.png')
+    plt.savefig(f'{save_dir}/slice_method_{method}_target_feature_{target_feature}.png')
 
     results = []
     for trial in study.trials:
@@ -126,7 +132,7 @@ def hyperparameter_tuning(method: str, n_trails: int, save_dir: str = 'result'):
     results_json = json.dumps(results, indent=4)
 
     # Save JSON data to a file
-    with open(f'{save_dir}/optuna_study_results_{method}.json', 'w') as f:
+    with open(f'{save_dir}/optuna_study_results_method_{method}_target_feature_{target_feature}.json', 'w') as f:
         f.write(results_json)
 
     best_trial = study.best_trial
@@ -139,4 +145,32 @@ def hyperparameter_tuning(method: str, n_trails: int, save_dir: str = 'result'):
 if __name__ == '__main__':
     method = sys.argv[1]
     n_trials = int(sys.argv[2])
-    hyperparameter_tuning(method, n_trials)
+
+    target_features = [
+        'S_DEMONSTRATIVE_WORD_BEFORE_NOUN', 'S_INDEFINITE_AFFIX', 'S_SEX_MARK', 'S_VOS', 'S_OBJECT_BEFORE_VERB',
+        'S_OBJECT_AFTER_VERB', 'S_NEGATIVE_WORD_AFTER_OBJECT', 'S_NUMCLASS_MARK', 'P_UVULAR_STOPS', 'P_CLICKS',
+        'S_CASE_SUFFIX', 'S_XVO', 'S_NEGATIVE_WORD_BEFORE_SUBJECT', 'P_UVULAR_CONTINUANTS', 'S_POLARQ_AFFIX',
+        'S_CASE_PROCLITIC', 'P_IMPLOSIVES', 'S_POLARQ_WORD', 'S_OBLIQUE_AFTER_VERB', 'S_DEFINITE_AFFIX',
+        'S_INDEFINITE_WORD', 'P_VELAR_NASAL', 'P_NASAL_VOWELS', 'S_RELATIVE_AROUND_NOUN', 'P_BILABIALS',
+        'S_POSSESSIVE_HEADMARK', 'S_SUBJECT_BEFORE_OBJECT', 'S_TEND_SUFFIX', 'P_TONE', 'S_OSV', 'P_VELAR_NASAL_INITIAL',
+        'S_GENDER_MARK', 'P_FRICATIVES', 'P_VOICED_PLOSIVES', 'S_DEMONSTRATIVE_PREFIX', 'S_SUBORDINATOR_WORD_BEFORE_CLAUSE',
+        'P_COMPLEX_CODAS', 'S_CASE_PREFIX', 'P_EJECTIVES', 'P_CODAS', 'S_OBLIQUE_BEFORE_VERB', 'S_OBJECT_HEADMARK',
+        'S_ADJECTIVE_AFTER_NOUN', 'S_DEMONSTRATIVE_SUFFIX', 'S_NEGATIVE_PREFIX', 'S_CASE_MARK', 'S_COMITATIVE_VS_INSTRUMENTAL_MARK',
+        'S_NEGATIVE_WORD_AFTER_VERB', 'S_SOV', 'S_NEGATIVE_WORD_ADJACENT_AFTER_VERB', 'S_NUMERAL_AFTER_NOUN', 'S_ADJECTIVE_WITHOUT_NOUN',
+        'S_PLURAL_PREFIX', 'S_DEGREE_WORD_AFTER_ADJECTIVE', 'S_POSSESSIVE_DEPMARK', 'S_ADPOSITION_AFTER_NOUN', 'S_SUBJECT_BEFORE_VERB',
+        'S_PLURAL_WORD', 'P_NASALS', 'S_TEND_PREFIX', 'S_SUBORDINATOR_WORD_AFTER_CLAUSE', 'S_OVS', 'S_NOMINATIVE_VS_ACCUSATIVE_MARK',
+        'S_DEGREE_WORD_BEFORE_ADJECTIVE', 'S_RELATIVE_AFTER_NOUN', 'S_SVO', 'S_OXV', 'P_LATERAL_L', 'P_LABIAL_VELARS', 'S_DEMONSTRATIVE_WORD_AFTER_NOUN',
+        'P_LATERAL_OBSTRUENTS', 'S_VSO', 'S_NEGATIVE_WORD_ADJACENT_BEFORE_VERB', 'S_POSSESSOR_AFTER_NOUN', 'P_PHARYNGEALS', 'P_VOICED_FRICATIVES',
+        'S_OBLIQUE_BEFORE_OBJECT', 'S_POLARQ_MARK_FINAL', 'S_PLURAL_SUFFIX', 'S_NEGATIVE_WORD_AFTER_SUBJECT', 'S_SUBJECT_AFTER_VERB',
+        'S_PERFECTIVE_VS_IMPERFECTIVE_MARK', 'S_NEGATIVE_WORD_FINAL', 'S_TAM_PREFIX', 'S_VOX', 'S_POSSESSIVE_PREFIX', 'P_GLOTTALIZED_RESONANTS',
+        'S_PROSUBJECT_CLITIC', 'P_TH', 'S_NEGATIVE_SUFFIX', 'S_DEFINITE_WORD', 'S_FUTURE_AFFIX', 'S_RELATIVE_BEFORE_NOUN', 'S_XOV',
+        'S_ERGATIVE_VS_ABSOLUTIVE_MARK', 'S_NEGATIVE_AFFIX', 'P_UVULARS', 'S_NUMERAL_BEFORE_NOUN', 'S_NEGATIVE_WORD_BEFORE_VERB', 'S_OVX',
+        'S_CASE_ENCLITIC', 'S_POLARQ_MARK_SECOND', 'S_ADJECTIVE_BEFORE_NOUN', 'S_PROSUBJECT_WORD', 'S_ADPOSITION_BEFORE_NOUN',
+        'S_POLARQ_MARK_INITIAL', 'S_NEGATIVE_WORD_INITIAL', 'S_TEND_DEPMARK', 'P_COMPLEX_ONSETS', 'S_POSSESSOR_BEFORE_NOUN',
+        'P_FRONT_ROUND_VOWELS', 'P_LATERALS', 'S_PAST_VS_PRESENT_MARK', 'S_SUBJECT_AFTER_OBJECT', 'S_TEND_HEADMARK', 'S_OBJECT_DEPMARK',
+        'S_NEGATIVE_WORD', 'S_TAM_SUFFIX', 'S_NEGATIVE_WORD_BEFORE_OBJECT', 'S_ANY_REDUP', 'S_PROSUBJECT_AFFIX', 'S_OBLIQUE_AFTER_OBJECT',
+        'S_POSSESSIVE_SUFFIX', 'P_VOICE', 'S_SUBORDINATOR_SUFFIX',
+    ]
+
+    for target_feature in target_features:
+        hyperparameter_tuning(method, n_trials, target_feature)
